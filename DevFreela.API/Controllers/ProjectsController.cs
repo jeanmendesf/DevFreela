3d5b8d29bfcb0e62d987af236763332886efc4b8
Jean Mendes <jeanmendesf@gmail.com>
2022-04-09 12:09:57 -0300

    Adicionando Actions em ProjectsController e criando models para Create e Update

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
 {
@@ -6,9 +7,33 @@ namespace DevFreela.API.Controllers
     public class ProjectsController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string query)
         {
             return Ok();
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody]CreateProjectModel createProject)
+        {
+            //Após criar o objeto, retornará o código 201 e mandará para a Action GetById
+            return CreatedAtAction(nameof(GetById), new {id = createProject.Id}, createProject);
+        }
+
+
+        //Devemos receber o id, para ver qual item será atualizado
+        //Devemos receber o item atualizado, para atualizar o BD
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel)
+        {
+            return NoContent();
         }
     }
 }
